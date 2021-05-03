6aa05ae227e888a26db2501022c9beaa5bf92d81
NaseerHines <Hinesnaseer@gmail.com>
2021-05-03 15:15:14 -0500

    added some db and api functions

--- server/api.js
+++ server/api.js
@@ -1,8 +1,41 @@
-const { Router } = require('express');
-const db = require('./db');
-
+const { Router } = require("express");
+const axios = require("axios");
+const { token } = require('../config');
+const { postUser, getUser, updateUser } = require('../db/index')
 const apiRouter = Router();
 
-// TODO: Your routes here!
+const getUser = (username, platform, cb) => {
+    // put in smiteAPI url
+    const apiUrl = ``;
+    const options = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    return axios
+    .get(apiUrl, options)
+    .then((account) => {
+        // fix this line after checking postman
+        const stats = account.data;
+        cb(stats);
+    })
+    .catch((err) => {
+        console.log(err, "didn't work");
+    });
+};
+
+apiRouter.post("/save", (req, res) => {
+    const { username, platform } = req.body;
+    console.log(username, platform);
+    getUser(username, platform, (data) => {
+    console.log(data);
+    postUser(data);
+    res.send(data);
+    });
+});
+
+apiRouter.patch("/update", () => {
+
+});
 
 module.exports.apiRouter = apiRouter;
--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const util = require('util');
 
 const DB_HOST = 'localhost';
 const DB_USER = 'root';
@@ -6,14 +7,40 @@ const DB_PASS = '';
 const DB_NAME = 'smiters';
 
 const connection = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
+  host: process.env.HOST || DB_HOST,
+  user: process.env.USER_NAME || DB_USER,
+  password: process.env.USER_PASS || DB_PASS,
+  database: process.env.DATABASE || DB_NAME,
 });
 
-const getItems = (callback) => {
-  // TODO: Your code here!
+const query = util.promisify(connection.query).bind(connection);
+
+connection.connect(err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Database connected!');
+  }
+});
+
+const postUser = (username, platform, kd, wl, playtime) => {
+  const mysqlQuery = 'INSERT INTO USERS VALUES(null, ?, ?, ?, ?, ?);';
+  return query(mysqlQuery, [username, platform, kd, wl, playtime]);
 };
 
-module.exports.getItems = getItems;
+const getUser = (username) => {
+  const mysqlQuery = 'SELECT * FROM USERS WHERE username = ?;';
+  return query(mysqlQuery, [username]);
+};
+
+const updateUser = (username, platform, kd, wl, playtime) => {
+  const mysqlQuery = 'UPDATE USERS SET kd = ?, wl = ?, playtime = ?;';
+  return query(mysqlQuery, [kd, wl, playtime]);
+};
+
+
+module.exports = {
+  postUser,
+  getUser,
+  updateUser
+};
